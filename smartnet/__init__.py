a7d3e02736ced9a09922203e997f49413dabd15d
liuxm06 <liuxm6111@126.com>
2019-12-17 08:22:24 +0800

    add source code

--- smartnet/__init__.py
+++ smartnet/__init__.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
+from .core.tensor import SmartTensor
+from .core.tensor_op import TensorOp
+from .core.graph import get_graph, no_grad
 
-# from .layers import *
-# from .optims import *
-# from .net import SmartNet
-# from .tensor import SmartTensor
